3e0a4f9e1135b0440acd41a57266d0a663bcf9b2
liuxingwt <90546778+liuxingwt@users.noreply.github.com>
2021-11-29 12:36:52 +0800

    Update train_example.sh

--- run/train_example.sh
+++ run/train_example.sh
@@ -9,6 +9,7 @@ mkdir -p ${OUTPUT}
 LOG=./logs/logs_${MOBILE_MEMORY}
 mkdir -p ${LOG}
 
+# For singel GPU, set --nproc_per_node=1
 python3 -u -m torch.distributed.launch --nproc_per_node=8 --master_port 11111 train.py \
     --img-root-dir  /home/data4/OULU \
     --train-file-path  ./data/list/p1_train_list.txt \
