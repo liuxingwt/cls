cc54a796e9e351e9b073b77d44c60cf6007cd52e
liuxingwt <90546778+liuxingwt@users.noreply.github.com>
2021-12-03 10:56:12 +0800

    Update test.sh

--- run/test.sh
+++ run/test.sh
@@ -1,2 +1,12 @@
-export CUDA_VISIBLE_DEVICES=9
-python3 ./test.py
+export CUDA_VISIBLE_DEVICES=0
+MOBILE_MEMORY=20211203
+
+OUTPUT=./results/out_${MOBILE_MEMORY}.txt/
+mkdir -p ${OUTPUT}
+
+
+python3 test.py  \
+--arch resnet10 \
+--valid_db "./test.txt" \
+--ckpt "./training-runs/checkpoint_20211129_2/checkpoint-100.pth.tar" \
+--out ${OUTPUT}
