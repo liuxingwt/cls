e88eb26537e4e5f6349c918813509674347f3a48
liuxingwt <90546778+liuxingwt@users.noreply.github.com>
2021-12-03 16:02:59 +0800

    Update metrics.py

--- tools/metrics.py
+++ tools/metrics.py
@@ -1,116 +1,95 @@
 """
-Liveness evaluation functions.
+Binary classification result evaluation functions.
+ROC curve reference: https://scikit-learn.org/stable/modules/generated/sklearn.metrics.roc_curve.html
+interpolation reference: https://numpy.org/doc/stable/reference/generated/numpy.interp.html
 """
-
 import os 
 import glob
 import numpy as np
-from scipy import interp  
+# import matplotlib.pyplot as plt
+from numpy import interp  
 from sklearn.metrics import roc_curve, auc, average_precision_score
-import matplotlib.pyplot as plt  
 
 
-recall_th = [0.1, 0.05, 0.01, 0.001, 0.0001]
-tpr_th = [0.8]
-hack_th = [0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 0.95]
+list_fpr = [0.1, 0.05, 0.01, 0.001, 0.0001]
+list_tpr = [0.9, 0.95, 0.99, 0.995, 0.999]
+list_thresh = [0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 0.95]
 
 
-def plot_show(y,prob):
-    fpr, tpr, thresholds = roc_curve(y, prob, pos_label=1) 
-    mean_tpr_list = []
-    for i in range(len(recall_th)):
-        mean_tpr_list.append(interp(recall_th[i], fpr, tpr))
-    for i in range(len(recall_th)):
-        print(i, 'tpr@fpr:', recall_th[i], mean_tpr_list[i]) 
-    
-    for i in range(len(hack_th)):
-        print(hack_th[i], 'fpr@tpr:', interp(hack_th[i], thresholds, tpr, period=1), interp(hack_th[i], thresholds, fpr, period=1), hack_th[i] ) 
+def eval_roc(score_file):
+    print('Evaluating Score File: ', score_file)
+    labels, scores = get_scores(score_file)
+    fpr, tpr, thresholds = roc_curve(labels, scores, pos_label=1) 
+    result_line = []
 
-    RightIndex=(tpr+(1-fpr)-1); 
-    right_index = np.argmax(RightIndex)
-    best_th = thresholds[right_index]
-    #best_th = 0.2
-    err = fpr[right_index]
-    mean_accuracy = 1-err
+    # interpolate tpr
+    print("index    TPR          FPR          Thresh")
+    for i in range(len(list_tpr)):
+        print("%3d"%(i+1),  "%10.4f"%list_tpr[i],  "%14.8f"%interp(list_tpr[i], tpr, fpr),  "%14.8f"%interp(list_tpr[i], tpr, thresholds))
 
-    return recall_th, mean_tpr_list
+    # interpolate fpr
+    print("index    FPR          TPR          Thresh")
+    for i in range(len(list_fpr)):
+        print("%3d"%(i+1),  "%10.4f"%list_fpr[i],  "%14.8f"%interp(list_fpr[i], fpr, tpr),  "%14.8f"%interp(list_fpr[i], fpr, thresholds))
 
-def _get_max_safety(num_split):
-    max_hack = '0.0'
-    for score_str in num_split[2:5]:
-        if float(score_str) >= float(max_hack):
-            max_hack = score_str
-    return max_hack
-    
+    # interpolate threshold
+    print("index   Thresh        FPR          TPR")
+    for i in range(len(list_thresh)):
+        print("%3d"%(i+1),  "%10.4f"%list_thresh[i], "%14.8f"%interp(list_thresh[i], thresholds, fpr, period=1), "%14.8f"%interp(list_thresh[i], thresholds, tpr, period=1)) 
+    return
+
+
 
 def get_scores(path_txt, isreverse = False):
-    fid = open(path_txt, 'r', encoding='utf-8')
-    lines = fid.readlines() 
-    print('Samples num:', len(lines))
-    fid.close()
+    with open(path_txt, 'r', encoding='utf-8') as fid:
+        lines = fid.readlines() 
+        # print('Samples num: ', len(lines))
 
-    test_scores = []#np.zeros([len(lines)])
-    test_labels = []#np.zeros([len(lines)])
+    test_scores = []    
+    test_labels = []
 
     for i in range(len(lines)):        
         line = lines[i]
         num_split = line.strip().split(' ')
         if len(num_split)==2:
             image_name = num_split[0]
-            # label = image_name[-1]
             score_str = num_split[-1]
+            # set label by image namee
+            if "fake" in image_name or "spoof" in image_name:
+                    label = "0"
+            elif "genuine" in image_name or "real" in image_name:
+                label = "1"
+            else:
+                pass
         elif len(num_split)>=3:
-            # num_split = line.strip().split(']')[0].strip().split(' ')
             image_name = num_split[0]
             label = num_split[1]
             score_str = num_split[-1]
         else:
             print('error score content!')
             exit(1)
-        '''
-        if 'real' in image_name: 
-            test_labels += [1]
-            test_scores += [float(score_str)]
-        elif True:
-            test_labels += [0]
-            test_scores += [float(score_str)]
-        else:
-            pass
-        '''
+
         if label == "0":
             test_labels += [0]
             test_scores += [float(score_str)]
         elif label == "1":
-            # 如果这些词在图片名中表示该图片为正例
             test_labels += [1]
             test_scores += [float(score_str)]
         else:
             print('error image name!')
             exit(1)
 
-
-    print('Total Test Samples:', len(test_scores))
+    print('Effective Test Samples:', len(test_scores))
     test_labels = np.array(test_labels)
     test_scores = np.array(test_scores)    
     
-    print('Good Test Samples:', np.sum(test_labels==1))
-    print('Bad Test Samples:', np.sum(test_labels==0))
+    print('Positive  Test Samples:', np.sum(test_labels==1))
+    print('Negative  Test Samples:', np.sum(test_labels==0))
     return test_labels, test_scores
 
 
-def eval_roc(score_file):
-    print('evaling score file:', score_file)
-    labels, scores = get_scores(score_file)
-    recall_th_list, mean_tpr_list = plot_show(labels, scores)
-
-    performance_str = ''
-    for _recal_th, _mean_tpr in zip(recall_th_list, mean_tpr_list):
-        performance_str += '%s=%.5f '%(str(_recal_th), _mean_tpr)
-    return performance_str 
-
-
-
+ 
 if __name__ == '__main__':
     
-    score_file = "/home/projects/list/test_result_transformer/checkpoint_20210524_epoch_2_cbsr_mas_v6_hifi-mask-test_bbox.txt"
+    score_file = "./results/out_20211203.txt"
     eval_roc(score_file)
