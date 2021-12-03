f4b74a59d27e38ad207f5a8518b09be5eebbe444
liuxingwt <90546778+liuxingwt@users.noreply.github.com>
2021-12-03 11:10:55 +0800

    Update test.sh

--- run/test.sh
+++ run/test.sh
@@ -1,12 +1,15 @@
 export CUDA_VISIBLE_DEVICES=0
-MOBILE_MEMORY=20211203
 
-OUTPUT=./results/out_${MOBILE_MEMORY}.txt/
+OUTPUT=./results/
 mkdir -p ${OUTPUT}
+
+MOBILE_MEMORY=20211203
+OUTPUTFILE=./${OUTPUT}/out_${MOBILE_MEMORY}.txt
+touch ${OUTPUTFILE}
 
 
 python3 test.py  \
 --arch resnet10 \
 --valid_db "./test.txt" \
 --ckpt "./training-runs/checkpoint_20211129_2/checkpoint-100.pth.tar" \
---out ${OUTPUT}
+--out ${OUTPUTFILE}
